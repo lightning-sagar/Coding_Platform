b1ea722d28cab00d5dd2531d88c4f7639bba8fa5
Sagar Seth <lightningsagar0@gmail.com>
2025-06-18 13:59:26 +0530

    added the backend url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       '/api':{
-        target:"http://localhost:5000/",
+        target:"https://coding-platform-backend-ol9u.onrender.com",
         changeOrigin:true
       }
     }
